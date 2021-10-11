f51d657550126919b59bc3b303114607a7607f72
Maxim Levandovskiy <90237902+oscuroplan@users.noreply.github.com>
2021-10-11 19:48:36 +0300

    Update autocheck4.js

--- autocheck4.js
+++ autocheck4.js
@@ -1,1 +1,29 @@
+//                                                                      ЗАДАЧА 1/48
+// Дополни код так, чтобы в переменной result был результат выполнения функции makePizza, а в переменной pointer была ссылка на функцию makePizza.
+function makePizza() {
+  return "Your pizza is being prepared, please wait.";
+}
+// Change code below this line
 
+const result = makePizza();
+const pointer = makePizza;
+
+//                                                                      ЗАДАЧА 2/48
+// Дополни функцию makeMessage так, чтобы она ожидала вторым параметром (параметр callback) колбэк-функцию и возвращала ее вызов.
+// Функция deliverPizza или makePizza будет передаваться как колбэк и ожидать аргументом имя готовой доставляемой пиццы.
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+// Chande code below this line
+function makeMessage(pizzaName, callback) {
+  return callback(pizzaName);
+}
+
+//                                                                      ЗАДАЧА 3/48
+// Дополни второй вызов функции makePizza(pizzaName, callback), передав вторым аргументом инлайн колбэк-функцию eatPizza(pizzaName),
+// которая логирует строку "Eating pizza <имя пиццы>".
