06b39cdbf599ad9d4820a506ad530318d1c6590d
Maxim Levandovskiy <90237902+oscuroplan@users.noreply.github.com>
2021-10-13 09:02:24 +0300

    Update autocheck4.js

--- autocheck4.js
+++ autocheck4.js
@@ -27,3 +27,16 @@ function makeMessage(pizzaName, callback) {
 //                                                                      ЗАДАЧА 3/48
 // Дополни второй вызов функции makePizza(pizzaName, callback), передав вторым аргументом инлайн колбэк-функцию eatPizza(pizzaName),
 // которая логирует строку "Eating pizza <имя пиццы>".
+function makePizza(pizzaName, callback) {
+  console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+  callback(pizzaName);
+}
+
+makePizza("Royal Grand", function deliverPizza(pizzaName) {
+  console.log(`Delivering pizza ${pizzaName}.`);
+});
+// Change code below this line
+
+makePizza("Ultracheese", function eatPizza(pizzaName) {
+console.log(`Eating pizza ${pizzaName}.`);
+});
